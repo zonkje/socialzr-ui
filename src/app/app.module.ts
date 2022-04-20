0bf633981bcbb05d5ae40245618fcd2d406b9d62
zonkje <szymon.radziszewski93@gmail.com>
2022-04-21 00:14:23 +0200

    add basic components. close #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostDetailComponent } from './posts/post-detail/post-detail.component';
+import { PostItemComponent } from './posts/post-list/post-item/post-item.component';
+import { ProfileComponent } from './profile/profile.component';
+import { GroupsComponent } from './groups/groups.component';
+import { ReportComponent } from './report/report.component';
+import { GroupListComponent } from './groups/group-list/group-list.component';
+import { GroupItemComponent } from './groups/group-list/group-item/group-item.component';
+import { GroupDetailComponent } from './groups/group-detail/group-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostsComponent,
+    PostListComponent,
+    PostDetailComponent,
+    PostItemComponent,
+    ProfileComponent,
+    GroupsComponent,
+    ReportComponent,
+    GroupListComponent,
+    GroupItemComponent,
+    GroupDetailComponent
   ],
   imports: [
     BrowserModule
